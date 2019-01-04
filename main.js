68a4d9d49b43d21e29ece9505f93283092dbb762
Ismail KHEIRY <ikheiry@sqli.com>
2019-01-04 17:03:23 +0100

    blockchain js tutoriel, v1.1

--- main.js
+++ main.js
@@ -7,17 +7,26 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString());
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log('Block mined: ' + this.hash);
     }
 }
 
 class BlockChain {
     constructor(){
         this.chain = [this.createGenesisBlock()];
-
+        this.difficulty = 2;
     }
 
     createGenesisBlock() {
@@ -30,8 +39,20 @@ class BlockChain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        //newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if(currentBlock.hash !== currentBlock.calculateHash() || currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
@@ -39,4 +60,13 @@ let kheiryCoin = new BlockChain();
 kheiryCoin.addBlock(new Block(1, "05/01/2019", {amount : 4}));
 kheiryCoin.addBlock(new Block(2, "06/01/2019", {amount : 14}));
 
-console.log(JSON.stringify(kheiryCoin, null, 4));
+//console.log(JSON.stringify(kheiryCoin, null, 4));
+
+console.log('IS blockchain valid? ' + kheiryCoin.isChainValid());
+
+kheiryCoin.chain[1].data.amount = 100;
+console.log('IS blockchain valid? ' + kheiryCoin.isChainValid());
+
+kheiryCoin.chain[1].data.amount = 110;
+kheiryCoin.chain[1].calculateHash();
+console.log('IS blockchain valid? ' + kheiryCoin.isChainValid());
